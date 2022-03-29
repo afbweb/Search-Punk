cd4d9cd9908ab216bf640996f01f99ce5c549b3a
Ariel <afbweb@hotmail.com>
2022-03-29 16:26:17 -0300

    first commit

---
+++ pages/Cartas/Carta.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Box, Badge, Img, Icon } from "@chakra-ui/react";
+
+const Carta = ({ carta }) => {
+  return (
+    <Box
+      marginTop="20px"
+      maxW="240"
+      borderWidth="1px"
+      borderRadius="lg"
+      overflow="hidden"
+      flex="grow"
+      border="1px solid rgb(229, 232, 235)"
+    >
+      <Img src={carta.imageUrl} />
+
+      <Box p="0"></Box>
+      <Box
+        p="2"
+        border-bottom-left-radius="10px"
+        border-bottom-right-radius="10px"
+        background="linear-gradient(rgba(229, 232, 235, 0.392) 0%, rgb(255, 255, 255) 20%)"
+        display="flex"
+        alignItems="baseline"
+      >
+        {carta.titulo}
+
+        <Box marginLeft="50px" >
+          <Img display="inline"
+            width="10px"
+            height="12px"
+            src="https://storage.opensea.io/files/6f8e2979d428180222796ff4a33ab929.svg"
+          />
+          {carta.precio}
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default Carta;
---
+++ pages/Cartas/Cartas.jsx
@@ -0,0 +1,102 @@
+import React, { useEffect, useState, useLayoutEffect } from "react";
+import Carta from "./Carta";
+import {
+  Box,
+  Grid,
+  Flex,
+  Button,
+  Input,
+  Wrap,
+  WrapItem,
+  Center,
+  LinkOverlay,
+  LinkBox,
+  Select,
+} from "@chakra-ui/react";
+import JSONDATA from "/data.json";
+
+// const cartas = JSONDATA;
+
+const CartasNFT = () => {
+  const [data, setData] = useState(JSONDATA);
+  const [buscar, setBuscar] = useState("");
+  const [orden, setOrden] = useState("ASC");
+  const sorting = (col) => {
+    if (orden === "ASC") {
+      const sorted = [...data].sort((a, b) =>
+        a[col].toLowerCase() > b[col].toLowerCase() ? 1 : -1
+      );
+      setData(sorted);
+      setOrden("DSC");
+    }
+    if (orden === "DSC") {
+      const sorted = [...data].sort((a, b) =>
+        a[col].toLowerCase() < b[col].toLowerCase() ? 1 : -1
+      );
+      setData(sorted);
+      setOrden("ASC");
+    }
+  };
+
+  return (
+    <>
+      <Center paddingTop="20px">
+        <Input
+        
+          variant="outline"
+          placeholder="Buscar por numero de Cryptopunk"
+          onChange={(e) => setBuscar(e.target.value)}
+        />
+      </Center>
+
+      <Box display="grid" paddingTop="20px">
+        <Button variant='outline'  size="lg" onClick={() => sorting("precio")}>
+          Ordenar por precio
+        </Button>
+        <Button variant='outline' size="lg" onClick={() => sorting("titulo")}>
+          Ordenar por nombre
+        </Button>
+      </Box>
+
+      <Wrap spacing="5px" justify="center">
+        {data
+          .filter((e) => {
+            if (buscar == "") {
+              return e;
+            } else if (
+              e.titulo.toLocaleLowerCase().includes(buscar.toLocaleLowerCase())
+            ) {
+              return e;
+            } 
+          })
+          .map((carta) => (
+            <LinkBox key={carta.id}>
+              <LinkOverlay href={carta.href}>
+                <WrapItem >
+                  <Center>
+                    <Carta carta={carta} />
+                  </Center>
+                </WrapItem>
+              </LinkOverlay>
+            </LinkBox>
+          ))}
+      </Wrap>
+    </>
+  );
+};
+
+export default CartasNFT;
+
+
+
+// .filter((e) => {
+//   if (buscar == "") {
+//     return e;
+//   } else if (
+//     e.titulo.toLocaleLowerCase().includes(buscar.toLocaleLowerCase())
+//   ) {
+//     return e;
+//   } else (
+//     "No"
+//   )
+// })
